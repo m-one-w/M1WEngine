cb7bba0b582c50006c74c1f7970724f550d5d8cd
owalk <oliverw56@gmail.com>
2023-02-17 12:19:59 -0500

    Add in support for tileset parsing

---
+++ game/spriteSheet.py
@@ -0,0 +1,41 @@
+import pygame
+
+
+class SpriteSheet:
+    """This class handles sprite sheets
+
+    Note: When calling images_at the rect is the format:
+    (x, y, x + offset, y + offset)
+    """
+
+    def __init__(self, filename):
+        """Load the sheet."""
+        try:
+            self.sheet = pygame.image.load(filename).convert()
+        except pygame.error as e:
+            print(f"Unable to load spritesheet image: {filename}")
+            raise SystemExit(e)
+
+    def image_at(self, rectangle, colorkey=None):
+        """Load a specific image from a specific rectangle."""
+        # Loads image from x, y, x+offset, y+offset.
+        rect = pygame.Rect(rectangle)
+        image = pygame.Surface(rect.size).convert()
+        image.blit(self.sheet, (0, 0), rect)
+        if colorkey is not None:
+            if colorkey is -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey, pygame.RLEACCEL)
+        return image
+
+    def images_at(self, rects, colorkey=None):
+        """Load multiple images and return them as a list."""
+        return [self.image_at(rect, colorkey) for rect in rects]
+
+    def load_strip(self, rect, image_count, colorkey=None):
+        """Load a strip of images, and return them as a list."""
+        tuples = [
+            (rect[0] + rect[2] * x, rect[1], rect[2], rect[3])
+            for x in range(image_count)
+        ]
+        return self.images_at(tuples, colorkey)
