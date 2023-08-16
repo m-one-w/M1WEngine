faa20bf145b59e8554e2de223bbc4a165485b136
owalk <oliverw56@gmail.com>
2023-08-16 18:43:17 -0400

    Added Tiled game data
    Filepaths for each Tiled level will be included in the game data.

---
+++ game/gameData.py
@@ -0,0 +1,8 @@
+level_1 = {
+    "ground": "levels/testing_level/testing-map_terrain.csv",
+    "rocks": "levels/testing_level/testing-map_rocks.csv",
+    "raised_ground": "levels/testing_level/testing-map_raised-ground.csv",
+    "plants": "levels/testing_level/testing-map_plants.csv",
+    "fence": "levels/testing_level/testing-map_fence.csv",
+    "extra": "levels/testing_level/testing-map_extra.csv",
+}
