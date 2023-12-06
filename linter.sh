ab12a8f7ca6e3fa914d49a7e35cb68649fb2cf07
owalk <oliverw56@gmail.com>
2023-12-06 14:05:28 -0500

    Fix linter script bug
    Flake8 was stalling out in certain python virtual enviroments causing the script to get stuck.

--- linter.sh
+++ linter.sh
@@ -1,11 +1,15 @@
-black .
-flake=$(flake8 . --count --max-complexity=15 --max-line-length=88 --statistics)
-style=$(pydocstyle . --count)
-if [[ "$flake" != 0 || "$style" != 0 ]]; then
+#!/bin/bash
+
+black game/
+style=`pydocstyle game/ --count`
+flake=`flake8 game/ --max-complexity=15 --max-line-length=88 --count`
+if [[ $flake != 0 || $style != 0 ]]; then
     echo "flake errors: $flake"
     echo "doc style errors: $style"
-    flake8 . --count --max-complexity=15 --max-line-length=88 --statistics
-    pydocstyle .
+    flake8 game/ --count --max-complexity=15 --max-line-length=88 --statistics
+    pydocstyle game/
     echo "press enter to continue"
     read
+else
+    exit 0
 fi
