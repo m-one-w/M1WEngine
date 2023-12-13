57ba75ff2ef8b7299b40a750e75d87d2f36a4e7c
= <nishi.sean@gmail.com>
2023-12-13 15:39:59 -0500

    213 update skeleton docs

--- game/tiles/entities/characters/NPCs/skeleton.py
+++ game/tiles/entities/characters/NPCs/skeleton.py
@@ -8,35 +8,34 @@ import settings
 class Skeleton(NPC):
     """First enemy class. Called Skeleton.
 
-    Inherits from Entity. Move method is overwritten using logic described
-    in the docs. Still uses Entity's collision_check method.
+    Inherits from NPC. Defines the skeleton specific logic.
 
     Attributes
     ----------
-    sprite_sheet: SpriteSheet
+    _sprite_sheet: SpriteSheet
         Hold the art assets info
-    image: pygame.Surface
+    _image: pygame.Surface
         Hold the current sprite image
-    rect: pygame.Rect
+    _rect: pygame.Rect
         Hold the current sprite position
-    hitbox: pygame.Rect
+    _hitbox: pygame.Rect
         Hold the hitbox rect
-    radar: pygame.Rect
+    _radar: pygame.Rect
         Hold the radar rect
-    obstacleSprites: pygame.sprite.Group
+    _obstacle_sprites: pygame.sprite.Group
         Hold the sprites that block movement
 
     Methods
     -------
     automate_movement(self)
-        Define how sprite will move
+        Define how skeleton will move
     collision_handler(self)
         Define how to handle collisions
-    skeleton_die(self)
+    die(self)
         Handle skeleton death actions
     update(self,  bad_sprites: pygame.sprite.Group,
     good_sprites: pygame.sprite.Group)
-        Define what methods are called on each tick
+        Update animation and move with collision handling
     """
 
     def __init__(
@@ -44,32 +43,42 @@ class Skeleton(NPC):
         pos: tuple,
         groups: pygame.sprite.Group,
         obstacle_sprites: pygame.sprite.Group,
-    ):
-        """Construct the skeleton class."""
+    ) -> None:
+        """Construct the skeleton class.
+
+        Parameters
+        ----------
+        pos: tuple
+            The spawning position in the map as (x,y) coordinates
+        groups: pygame.sprite.Group
+            The sprite groups this skeleton is a part of
+        obstacle_sprites: pygame.sprite.Group
+            The sprites the skeleton cannot move through
+        """
         super().__init__(groups)
 
         # grab self image
-        skeletonMovementsPath = "graphics/skeleton/skeleton.png"
-        self.sprite_sheet = SpriteSheet(skeletonMovementsPath, pygame.Color("black"))
-        skeletonSelfImageRect = pygame.Rect(
+        skeleton_sprite_path = "graphics/skeleton/skeleton.png"
+        self._sprite_sheet = SpriteSheet(skeleton_sprite_path, pygame.Color("black"))
+        size_of_single_sprite = pygame.Rect(
             0, 0, settings.ENTITY_WIDTH, settings.ENTITY_HEIGHT
         )
-        self.image = self.sprite_sheet.image_at(skeletonSelfImageRect)
+        self.image = self._sprite_sheet.image_at(size_of_single_sprite)
         self.rect = self.image.get_rect(topleft=pos)
-        self.obstacle_sprites = obstacle_sprites
+        self._obstacle_sprites = obstacle_sprites
         self.import_assets()
-        self.hitbox = self.rect.inflate(-4, 0)
+        self._hitbox = self.rect.inflate(-4, 0)
 
-    def automate_movement(self):
+    def automate_movement(self) -> None:
         """Movement logic method."""
         # update radar with new pos
-        self.radar.center = self.rect.center
+        self._radar.center = self.rect.center
         # always patrol when no radar detections
         passive_state = self.set_state_patrol
 
         # change state to flee if player is nearby
         active_state = self.set_state_flee
-        self.radar_set_states(self.player, active_state, passive_state)
+        self.radar_set_states(self._player, active_state, passive_state)
 
         # change state to attack if there is good_entity nearby
         active_state = self.set_state_attack
@@ -77,10 +86,10 @@ class Skeleton(NPC):
 
         self.move_based_on_state()
 
-    def collision_handler(self):
+    def collision_handler(self) -> None:
         """Handle collision interactions with environment."""
         super().collision_handler()
-        collisions = self.rect.colliderect(self.player.hitbox)
+        collisions = self.rect.colliderect(self._player._hitbox)
 
         if collisions:
             try:
@@ -88,14 +97,14 @@ class Skeleton(NPC):
             except ValueError:
                 print("Unable to resolve player collisions from skeleton.")
 
-    def die(self):
+    def die(self) -> None:
         """Handle actions on skeleton death."""
         super().die()
         self.scoreController.bad_entity_destroyed_update_score(self.__class__.__name__)
 
     def update(
         self, bad_sprites: pygame.sprite.Group, good_sprites: pygame.sprite.Group
-    ):
+    ) -> None:
         """Update skeleton behavior based on entities on the map.
 
         Reaction logic is described in the [documentation](https://github.com/Sean-Nishi/Lunk-Game/blob/main/docs/specSheet.md#player-movement).# noqa: E501
@@ -103,9 +112,9 @@ class Skeleton(NPC):
         Parameters
         ----------
         bad_sprites: pygame.sprite.Group()
-            Group of entities hostile to the player
+            Group of bad aligned sprites
         good_sprites: pygame.sprite.Group()
-            Group of entities friendly to the player
+            Group of good aligned sprites
         """
         self._bad_sprites = bad_sprites
         self._good_sprites = good_sprites
@@ -114,6 +123,6 @@ class Skeleton(NPC):
         # will move half as fast as player at the same speed
         self.automate_movement()
 
-        if self.current_state != self.states.Thrown:
+        if self._current_state != self._states.Thrown:
             # Flee collision handler must be handled within auto movement
             self.collision_handler()
