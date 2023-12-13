5d0beb45374dc5d382c3906b2cb6bf32f1ca33aa
owalk <oliverw56@gmail.com>
2023-12-13 15:32:25 -0500

    Add basic entity class

---
+++ game/tiles/entities/entity.py
@@ -0,0 +1,21 @@
+"""This module contains the Entity class."""
+import pygame
+from tiles.tile import Tile
+
+
+class Entity(Tile):
+    """Entity class.
+
+    Base class for all entities including.
+    """
+
+    def __init__(self, groups: pygame.sprite.Group):
+        """Initialize an entity.
+
+        Parameters
+        ----------
+        groups: pygame.sprite.Group
+            The groups this entity is a part of
+        """
+        super().__init__(groups)
+        # TODO: implement the rest of the entity specific logic
