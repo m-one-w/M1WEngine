f8da0c8e3427025cb9d4b286789c5e09ec9bfc39
= <nishi.sean@gmail.com>
2023-12-14 14:18:46 -0500

    214 update player docs

--- game/tiles/entities/characters/player.py
+++ game/tiles/entities/characters/player.py
@@ -14,24 +14,23 @@ MAX_ACTION_QUEUE_LENGTH = 3
 
 
 class Player(Character):
-    """Player class which contains the object players will directly control.
+    """Player class contains the object players will directly control.
 
-    The player class will handle movement logic and sprite changing logic for the player
-    object. The player class will also handle any collision logic that influences the
-    player object.
+    The player class will handle user input, movement logic, and logic for powers.
+    The player class will also handle the action queue for said powers.
 
     Attributes
     ----------
-    sprite_sheet: SpriteSheet
+    _sprite_sheet: SpriteSheet
         Hold all the player animations
-    image: pygame.Surface
-        Hold the current player image
-    rect: pygame.Rect
+    _image: pygame.Surface
+        Hold the current player animation image
+    _rect: pygame.Rect
         Hold the player position and size
-    hitbox: pygame.Rect
+    _hitbox: pygame.Rect
         Hold the player hitbox
-    obstacle_sprites: pygame.sprite.Group
-        Contain which sprites are considered opbstacles
+    _obstacle_sprites: pygame.sprite.Group
+        Hold the sprites that are considered obstacles
     _action_queue: list
         Contain the internal player action queue
 
@@ -39,22 +38,22 @@ class Player(Character):
     -------
     eaten_power(self)
         Get the current eaten power
-    eaten_power(self, new_value)
+    eaten_power(self, new_value) -> EatenPowers
         Set the current eaten power
     input(self)
         Handle keyboard input for the player
     ensure_full_action_queue(self)
         Ensure the action queue is growing if less than max length
-    current_action(self)
+    current_action(self) -> Actions
         Get the current player action
-    pop_next_player_action(self)
+    pop_next_player_action(self) -> Actions
         Get and remove the next player action
     move(self, speed)
-        Player specific move
+        Player specific movement logic
     update(
         self, bad_sprites: pygame.sprite.Group, good_sprites: pygame.sprite.Group
     )
-        Update all player data
+        Update data, get user input, animate, rotate, handle collisions, and movement
     """
 
     def __init__(
@@ -62,69 +61,96 @@ class Player(Character):
         pos: tuple,
         groups: pygame.sprite.Group,
         obstacle_sprites: pygame.sprite.Group,
-    ):
+    ) -> None:
         """Construct the player object.
 
         Sets all required values to construct the player object.
+
+        Parameters
+        ----------
+        pos: tuple
+            The (x, y) position the player spawns at
+        groups: pygame.sprite.Group
+            The sprite groups the player is part of
+        obstacle_sprites: pygame.sprite.Group
+            The sprite group containing all sprites the player can't move through
         """
         super().__init__(groups)
 
         # grab self image
         playerMovementsPath = "graphics/player/playerWalking.png"
-        self.sprite_sheet = SpriteSheet(playerMovementsPath, pygame.Color("black"))
-        playerSelfImageRect = pygame.Rect(
+        self._sprite_sheet = SpriteSheet(playerMovementsPath, pygame.Color("black"))
+        player_image_rect = pygame.Rect(
             0, 0, settings.ENTITY_WIDTH, settings.ENTITY_HEIGHT
         )
-        self.image: pygame.surface = self.sprite_sheet.image_at(playerSelfImageRect)
-        self.rect = self.image.get_rect(topleft=pos)
+        self._image: pygame.surface = self._sprite_sheet.image_at(player_image_rect)
+        self._rect = self._image.get_rect(topleft=pos)
         # ensure hitbox is large enough to cover player rotations
-        self.hitbox = self.rect.inflate(2, 0)
+        self._hitbox = self._rect.inflate(2, 0)
 
-        self.obstacle_sprites = obstacle_sprites
+        self._obstacle_sprites = obstacle_sprites
         self.import_assets()
+
+        # load player powers
         self._action_queue: list = []
-        self.eaten_power = EatenPowers.empty_stomach
+        self._eaten_power = EatenPowers.empty_stomach
 
     @property
     def eaten_power(self) -> EatenPowers:
-        """Get the current eaten power of the player."""
+        """Get the current eaten power of the player.
+
+        Returns
+        -------
+        eaten_power: EatenPowers
+            The current EatenPowers enum value
+        """
         return self._eaten_power
 
     @eaten_power.setter
-    def eaten_power(self, new_value):
+    def eaten_power(self, new_value) -> None:
         """Set the new eaten power.
 
         Parameters
         ----------
         new_value: int
-            Incoming value to set which must be a EatenPowers type
+            Incoming value to set which must be an EatenPowers type
+
+        Raises
+        ------
+        ValueError: new_value was not in EatenPowers enum
         """
         if isinstance(new_value, EatenPowers):
             self._eaten_power = new_value
         else:
             raise ValueError("Player can only consume eaten_power values.")
 
-    def input(self):
-        """Input function to handle keyboard input to the player class.
+    def input(self) -> None:
+        """Handle keyboard input to the player class.
 
-        This function will handle turning the player object as input is received.
+        This method will handle turning the player object as input is received.
         """
         keys = pygame.key.get_pressed()
 
         # left/right input
         if keys[pygame.K_LEFT]:
-            self.compass.rotate_ip(-PLAYER_ROTATION_SPEED)
+            self._compass.rotate_ip(-PLAYER_ROTATION_SPEED)
         elif keys[pygame.K_RIGHT]:
-            self.compass.rotate_ip(PLAYER_ROTATION_SPEED)
+            self._compass.rotate_ip(PLAYER_ROTATION_SPEED)
 
-    def ensure_full_action_queue(self):
+    def ensure_full_action_queue(self) -> None:
         """Fill up the current action queue up to a limit."""
         if len(self._action_queue) < MAX_ACTION_QUEUE_LENGTH:
             actions = [Actions.consume, Actions.destroy, Actions.throw]
             self._action_queue.append(random.choice(actions))
 
     def current_action(self) -> Actions:
-        """Get the current action."""
+        """Get the current action.
+
+        Returns
+        -------
+        current_action: Actions
+            The Actions enum value of the current action
+        """
         current_action = Actions.none
         if len(self._action_queue) > 0:
             current_action = self._action_queue[-1]
@@ -137,18 +163,28 @@ class Player(Character):
             current_action = self._action_queue.pop()
         return current_action
 
-    def move(self, speed):
+    def move(self, speed) -> None:
         """Player specific move function."""
         super().move(speed)
         # adjust the hitbox for optimal sprite coverage
-        self.hitbox.centerx = self.rect.centerx + 1
+        self._hitbox.centerx = self._rect.centerx + 1
 
     def update(
         self, bad_sprites: pygame.sprite.Group, good_sprites: pygame.sprite.Group
-    ):
+    ) -> None:
         """Update player behavior based on player input.
 
         Controls and movement logic is described in the [documentation](https://github.com/Sean-Nishi/Lunk-Game/blob/main/docs/specSheet.md#player-movement).# noqa: E501
+        Updates local good and bad sprites, handles user input,
+        ensures full action queue, sets status by current rotation,
+        gets next animation image, handles collisions, and moves the player
+
+        Parameters
+        ----------
+        bad_sprites: pygame.sprite.Group
+            The sprite group containing all bad aligned characters
+        good_sprites: pygame.sprite.Group
+            The sprite group containing all good aligned characters
         """
         self._bad_sprites = bad_sprites
         self._good_sprites = good_sprites
@@ -156,8 +192,8 @@ class Player(Character):
         self.ensure_full_action_queue()
         self.set_status_by_curr_rotation()
         image = self.animate()
-        self.image = self.set_image_rotation(image)
+        self._image = self.set_image_rotation(image)
         # a new direction may be set by the collision handler
         self.collision_handler()
         # will move twice as fast as any other entity at the same speed due to camera.
-        self.move(self.speed)
+        self.move(self._speed)
