95094f458d168c52804c86fbab7a6d93eba7c03f
= <nishi.sean@gmail.com>
2023-12-19 15:10:56 -0500

    299 update support.py docs and add typehints

--- game/filemanagement/support.py
+++ game/filemanagement/support.py
@@ -1,20 +1,24 @@
 """This module contains methods to load game assets."""
 from csv import reader
 from os import walk
-from typing import List
 import pygame
 from settings import TILESIZE
 
 
-def import_csv_layout(path: str) -> List[int]:
+def import_csv_layout(path: str) -> list[int]:
     """Read in csv values into an array.
 
     Parameters
     ----------
     path: str
         The filepath to read from
+
+    Returns
+    -------
+    list[int]
+        The list of Tile values that represent the map layout
     """
-    terrain_map = []
+    terrain_map: list = []
     with open(path) as level_map:
         layout = reader(level_map, delimiter=",")
         for row in layout:
@@ -22,7 +26,7 @@ def import_csv_layout(path: str) -> List[int]:
         return terrain_map
 
 
-def import_folder(path: str):
+def import_folder(path: str) -> None:
     """Import all the surfaces in a directory.
 
     Parameters
@@ -30,34 +34,39 @@ def import_folder(path: str):
     path: str
         The filepath to import from
     """
-    surface_list = []
+    surface_list: list = []
     # import all images from a folder
     for _, __, image_files in walk(path):
         for image in image_files:
-            full_path = path + "/" + image
-            image_surface = pygame.image.load(full_path).convert_alpha()
+            full_path: str = path + "/" + image
+            image_surface: pygame.Surface = pygame.image.load(full_path).convert_alpha()
             surface_list.append(image_surface)
 
 
-def import_cut_graphic(path: str) -> List[pygame.Surface]:
+def import_cut_graphic(path: str) -> list[pygame.Surface]:
     """Cut a tileset into correct sprites.
 
     Parameters
     ----------
     path: str
         The filepath to find the tileset
-    """
-    surface = pygame.image.load(path).convert_alpha()
-    tile_num_x = int(surface.get_size()[0] / TILESIZE)
-    tile_num_y = int(surface.get_size()[1] / TILESIZE)
 
-    cut_tiles = []
+    Returns
+    -------
+    list[pygame.Surface]
+        The list of images extracted from a larger image
+    """
+    surface: pygame.Surface = pygame.image.load(path).convert_alpha()
+    tile_num_x: int = int(surface.get_size()[0] / TILESIZE)
+    tile_num_y: int = int(surface.get_size()[1] / TILESIZE)
+
+    cut_tiles: list = []
     for row in range(tile_num_y):
         for col in range(tile_num_x):
-            x = col * TILESIZE
-            y = row * TILESIZE
-            new_surface = pygame.Surface((TILESIZE, TILESIZE))
-            new_rect = pygame.Rect(x, y, TILESIZE, TILESIZE)
+            x: int = col * TILESIZE
+            y: int = row * TILESIZE
+            new_surface: pygame.Surface = pygame.Surface((TILESIZE, TILESIZE))
+            new_rect: pygame.Rect = pygame.Rect(x, y, TILESIZE, TILESIZE)
             new_surface.blit(surface, (0, 0), new_rect)
             cut_tiles.append(new_surface)
     return cut_tiles
