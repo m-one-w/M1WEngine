1987d946bf78b11670cb9628112e47b09a4c07bd
= <nishi.sean@gmail.com>
2023-12-19 15:05:04 -0500

    298 updated spriteSheet docs and add typehints

--- game/filemanagement/spriteSheet.py
+++ game/filemanagement/spriteSheet.py
@@ -1,6 +1,5 @@
 """This module contains the SpriteSheet class."""
 import pygame
-from typing import List, Tuple
 
 
 class SpriteSheet:
@@ -8,40 +7,90 @@ class SpriteSheet:
 
     Note: When calling images_at the rect is the format:
     (x, y, x + offset, y + offset)
+
+    Attributes
+    ----------
+    _color_key: pygame.Color
+        The background color for the sprite
+    _sheet: pygame.Surface
+        The animation sheet to display
     """
 
-    def __init__(self, filename: str = None, color_key: pygame.Color | Tuple = None):
-        """Load the sheet."""
+    def __init__(
+        self, image_path: str = None, color_key: pygame.Color | tuple = None
+    ) -> None:
+        """Load the sheet.
+
+        file_name: str
+            The path to the image to load
+        color_key: pygame.Color | Tuple
+            The color to make the background transparent
+        """
         if isinstance(color_key, pygame.Color):
-            self._color_key = (color_key.r, color_key.g, color_key.b)
+            self._color_key: tuple = (color_key.r, color_key.g, color_key.b)
         else:
-            self._color_key = color_key
-        if filename:
+            self._color_key: pygame.Color = color_key
+        if image_path:
             try:
-                self._sheet = pygame.image.load(filename).convert()
+                self._sheet: pygame.Surface = pygame.image.load(image_path).convert()
             except pygame.error as e:
-                print(f"Unable to load spritesheet image: {filename}")
+                print(f"Unable to load spritesheet image: {image_path}")
                 raise SystemExit(e)
 
     def image_at(self, rectangle: tuple) -> pygame.Surface:
-        """Load a specific image from a specific rectangle."""
+        """Load a specific image from a specific rectangle.
+
+        Parameters
+        ----------
+        rectangle: tuple
+            The tuple containing the length and width of image rectangle
+
+        Returns
+        -------
+        image: pygame.Surface
+            The surface containing the image of the specified rect from the animations
+        """
         # Loads image from x, y, x+offset, y+offset.
-        rect = pygame.Rect(rectangle)
-        image = pygame.Surface(rect.size)
+        rect: pygame.Rect = pygame.Rect(rectangle)
+        image: pygame.Surface = pygame.Surface(rect.size)
         if self._sheet:
             image.blit(self._sheet, (0, 0), rect)
             image.set_colorkey(self._color_key)
             return image
         else:
-            print("atal ERROR!! no sprite sheet was set!")
+            print("Fatal ERROR!! No sprite sheet was set!")
 
-    def images_at(self, rects: List[Tuple[int]]) -> List[pygame.Surface]:
-        """Load multiple images and return them as a list."""
+    def images_at(self, rects: list[tuple[int]]) -> list[pygame.Surface]:
+        """Load multiple images and return them as a list.
+
+        Parameters
+        ----------
+        rects: list[tuple[int]]
+            The list of rects containing images to go through
+
+        Returns
+        -------
+        list[pygame.Surface]
+            The list of images that were just loaded
+        """
         return [self.image_at(rect) for rect in rects]
 
-    def load_strip(self, rect, image_count: int) -> List[pygame.Surface]:
-        """Load a strip of images, and return them as a list."""
-        tuples = [
+    def load_strip(self, rect: pygame.Rect, image_count: int) -> list[pygame.Surface]:
+        """Load a strip of images, and return them as a list.
+
+        Parameters
+        ----------
+        rect: pygame.Rect
+            The rectangle dimensions for the images
+        image_count: int
+            The number of images to load
+
+        Returns
+        -------
+        list[pygame.Surface]
+            All loaded images as a list
+        """
+        tuples: list[tuple] = [
             (rect[0] + rect[2] * x, rect[1], rect[2], rect[3])
             for x in range(image_count)
         ]
