c7b12a29cdae6222ce49df952249751def73e220
= <nishi.sean@gmail.com>
2024-01-10 20:33:03 -0500

    item.py docstrings and typehints

--- game/tiles/entities/items/item.py
+++ game/tiles/entities/items/item.py
@@ -10,41 +10,40 @@ class Item(Entity):
 
     Base class for all Items in the game.
 
-    Attributes
-    ----------
-    _sprite_sheet: SpriteSheet
-        Contain sprite image information
-    _status: str
-        Contain current status
-    _animation_speed: float
-        Contain current animation speed
+    Methods
+    -------
+    import_assets(self) -> None
+        Divide the animation image into smaller parts. Calls parent's import_assets
+    update(self) -> None
+        Update the item's image
     """
 
     def __init__(
         self,
         group: pygame.sprite.Group,
-        pos: tuple,
+        pos: tuple[int, int],
         image_path: str,
         image_rect: pygame.Rect,
-    ):
+    ) -> None:
         """Initialize a tile.
 
         Parameters
         ----------
         group: pygame.sprite.Group
             The groups this sprite is a part of
+        pos: tuple[int, int]
+            The starting (x, y) coordinates
+        image_path: str
+            The path to the item's images
+        image_rect: pygame.Rect
+            The size of the item
         """
         super().__init__(group, pos, image_path, image_rect)
-        self._status: str = "idle"
-        self._animation_speed: float = 0.05
+        self._status = "idle"
+        self._animation_speed = 0.05
 
-    def import_assets(self):
-        """Import and divide the animation image into it's smaller parts.
-
-        Called at end of :func:'init()', takes the image with all character animations
-        and divides it into its sub-images. Can be expanded with more images to fulfill
-        idle and attack animations.
-        """
+    def import_assets(self) -> None:
+        """Import and divide the animation image into it's smaller parts."""
         idle_animation: AnimationDict = {
             "name": "idle",
             "image_rect": (
@@ -55,9 +54,9 @@ class Item(Entity):
             ),
             "image_count": 3,
         }
-        self._animation_dict: list[AnimationDict] = [idle_animation]
+        self._animation_dict = [idle_animation]
         super().import_assets()
 
-    def update(self):
+    def update(self) -> None:
         """Update the item instance."""
         self.image = self.animate()
