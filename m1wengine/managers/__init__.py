c01db164ec7d0cd2bc0619f42e04db93ef57cebd
owalk <oliverw56@gmail.com>
2024-01-16 15:17:25 -0500

    Add __init__ files to each modole This allows setup.py to detect code within the directory structure.

---
+++ m1wengine/managers/__init__.py
@@ -0,0 +1,4 @@
+"""Package for m1wengine game engine class managers.
+
+These managers are used to control different facets of the game logic.
+"""
---
+++ m1wengine/menus/main_menu/__init__.py
@@ -0,0 +1,1 @@
+"""Package for m1wengine game engine main menu."""
