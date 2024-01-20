10363eb8ee1fc5781c149a7823f959482f58baab
owalk <oliverw56@gmail.com>
2024-01-20 18:22:40 -0500

    Update linter script

--- linter.sh
+++ linter.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-black game/
-style=`pydocstyle game/ --count`
-flake=`flake8 game/ --max-complexity=15 --max-line-length=88 --count`
+black m1wengine/
+style=`pydocstyle m1wengine/ --count`
+flake=`flake8 m1wengine/ --max-complexity=15 --max-line-length=88 --count`
 if [[ $flake != 0 || $style != 0 ]]; then
     echo "flake errors: $flake"
     echo "doc style errors: $style"
-    flake8 game/ --count --max-complexity=15 --max-line-length=88 --statistics
-    pydocstyle game/
+    flake8 m1wengine/ --count --max-complexity=15 --max-line-length=88 --statistics
+    pydocstyle m1wengine/
     echo "press enter to continue"
     read
 else
