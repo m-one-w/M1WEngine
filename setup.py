90216ae342d98ef9e2828d5eb7c9dbea442a72df
owalk <oliverw56@gmail.com>
2024-01-20 18:14:10 -0500

    Add setup.py
    Allows for building of PyPi wheels.

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""This module is for the package setup."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="m1wengine",
+    version="1.0.0",
+    description="A game engine built on top of pygame",
+    url="https://github.com/Sean-Nishi/M1Wengine",
+    packages=find_packages(),
+    install_requires=["pygame-menu==4.4.3", "pygame==3.11.1"],
+    extra_requires={
+        "dev": ["black==23.1.0", "flake8==4.0.1", "pydocstyle==6.3.0", "twine>=4.0.2"]
+    },
+    python_requires=">=3.11",
+)
