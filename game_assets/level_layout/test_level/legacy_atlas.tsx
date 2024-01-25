02c0ea3066f8c798975f7bcfcec408dcc0cf4139
owalk <oliverw56@gmail.com>
2024-01-25 15:26:31 -0500

    Rm stale game assets directory

--- game_assets/level_layout/test_level/legacy_atlas.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="legacy_atlas" tilewidth="16" tileheight="16" tilecount="9620" columns="74">
- <image source="../../graphics/tilesets/legacy_atlas.png" width="1184" height="2080"/>
-</tileset>
--- game_assets/level_layout/test_level/original_atlas.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="original_atlas" tilewidth="16" tileheight="16" tilecount="8960" columns="112">
- <image source="../../graphics/tilesets/original_atlas.png" width="1792" height="1280"/>
-</tileset>
