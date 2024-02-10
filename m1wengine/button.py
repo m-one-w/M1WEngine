d91d03430eddc7f295d5196741d3c4748918d968
fcpolonia <z21comma@proton.me>
2024-02-10 16:41:52 +0100

    Allow buttons to have multiple display text
    a

--- m1wengine/button.py
+++ m1wengine/button.py
@@ -49,7 +49,8 @@ class Button(pygame.sprite.Sprite):
         y: int,
         width: int,
         height: int,
-        button_text: str = "Button",
+        text,
+        splitter: str = '',
         on_click_function: callable = None,
         one_press: bool = False,
     ):
@@ -65,18 +66,35 @@ class Button(pygame.sprite.Sprite):
             The width of the button
         height: int
             The height of the button
-        button_text
-            The string to display on the button
+        text: str
+            The string or list to be displayed on the button
+        splitter: str
+            a char that would split the string into separate elements
         on_click_function: callable
             The method to run when the button is clicked
         one_press: bool
             Should the button click once or call repeatedly when held
         """
+
         super().__init__()
+        # Splits text into a list
+        self.index = 0
+        self.Button_Texts_List = []
+        self.button_text = ''
+        if type(text) == str:
+            if splitter != '':
+                self.Button_Texts_List = text.split(splitter)
+            else:
+                self.Button_Texts_List = [text]
+        elif type(text) == list:
+            self.Button_Texts_List = text
+        self.button_text = self.Button_Texts_List[self.index]
+        # Dimensions
         self.__x: int = x
         self.__y: int = y
         self.__width: int = width
         self.__height: int = height
+        # Clicking functionaliti
         self._on_click_function: callable = on_click_function
         self.__on_press: bool = one_press
         self.__already_pressed: bool = False
@@ -84,13 +102,20 @@ class Button(pygame.sprite.Sprite):
         self.rect: pygame.Rect = pygame.Rect(
             self.__x, self.__y, self.__width, self.__height
         )
-        self.__text: Text = Text(x, y, button_text)
+        self.__text: Text = Text(x, y, self.button_text)
 
         self.__fill_colors: dict[str, str] = {
             "normal": "#ffffff",
             "hover": "#666666",
             "pressed": "#333333",
         }
+
+    def iterate_text_list(self):
+        """Increase the index to change the displayed text and reset index if its already on the last element"""
+        if self.Button_Texts_List[self.index] == self.Button_Texts_List[-1]:
+            self.index = 0
+        else:
+            self.index += 1
 
     def update(self):
         """Update the state of the button as needed."""
@@ -109,3 +134,4 @@ class Button(pygame.sprite.Sprite):
                 self.__already_pressed = False
         self.__text.render_font()
         self.image.blit(self.__text.image, (0, 0))
+
