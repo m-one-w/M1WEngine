06ab7b35e3b4c716a4483509bfa49230f6ade137
= <nishi.sean@gmail.com>
2024-02-21 13:29:20 -0500

    cleaned up score_controller.py

--- m1wengine/score_controller.py
+++ m1wengine/score_controller.py
@@ -8,6 +8,8 @@ class ScoreController(object):
 
     Attributes
     ----------
+    score_dictionary: static dict[str, dict[str, int]]
+        The NPC death score and boredom meter numbers
     _current_score: int
         The current score for the game
     _boredom_meter: int
@@ -21,11 +23,11 @@ class ScoreController(object):
         Initialize the singleton object's starting instance
     current_score(self)
         Get the current score
-    current_score(self, new_value)
+    current_score(self, new_value: int)
         Set the current score
     boredom_meter(self)
         Get the boredom meter
-    boredom_meter(self, new_value)
+    boredom_meter(self, new_value: int)
         Set the boredom meter
     """
 
@@ -50,12 +52,10 @@ class ScoreController(object):
             cls.instance = super(ScoreController, cls).__new__(cls)
         return cls.instance
 
-    def __init__(
-        self,
-    ):
+    def __init__(self):
         """Construct the first singleton instance."""
         self._current_score: int = 0
-        self._boredom_meter: int = 0
+        self._boredom_meter: int = 100
 
     @property
     def current_score(self) -> int:
@@ -63,15 +63,19 @@ class ScoreController(object):
         return self._current_score
 
     @current_score.setter
-    def current_score(self, new_value):
+    def current_score(self, new_value: int):
         """Set the new current score.
 
         Parameters
         ----------
         new_value: int
             New incoming value to set
+
+        Raises
+        ------
+        ValueError: the current score must be a non-negative integer
         """
-        if new_value >= 0:
+        if isinstance(new_value, int) and new_value >= 0:
             self._current_score = new_value
         else:
             raise ValueError("Current score cannot be a negative value.")
@@ -82,20 +86,24 @@ class ScoreController(object):
         return self._boredom_meter
 
     @boredom_meter.setter
-    def boredom_meter(self, new_value):
+    def boredom_meter(self, new_value: int):
         """Set the new boredom meter.
 
         Parameters
         ----------
         new_value: int
             New incoming value to set
+
+        Raises
+        ------
+        ValueError: Boredom meter must be a non-negative integer
         """
-        if new_value >= 0:
+        if isinstance(new_value, int) and new_value >= 0:
             self._boredom_meter = new_value
         else:
             raise ValueError("Boredom meter cannot be a negative value.")
 
-    def bad_entity_destroyed_update_score(self, entity_name: str):
+    def bad_entity_destroyed_update_score(self, entity_name: str) -> None:
         """Update meta data on bad entity destroyed.
 
         Checks each individual entity when making updates.
@@ -104,6 +112,10 @@ class ScoreController(object):
         ----------
         entity_name: str
             The name of the entity
+
+        Raises
+        ------
+        ValueError: entity_name was not found in the score_dictionary
         """
         check = entity_name
         if check == "Skeleton":
@@ -112,8 +124,10 @@ class ScoreController(object):
         elif check == "Minotaur":
             self.current_score += self.score_dictionary["minotaur_death"]["score"]
             self.boredom_meter += self.score_dictionary["minotaur_death"]["boredom"]
+        else:
+            raise ValueError("Unknown entity type. Not found in score_dictionary.")
 
-    def good_entity_destroyed_update_score(self, entity_name: str):
+    def good_entity_destroyed_update_score(self, entity_name: str) -> None:
         """Update meta data on good entity destroyed.
 
         Checks each individual entity when making updates.
@@ -122,6 +136,10 @@ class ScoreController(object):
         ----------
         entity_name: str
             The name of the entity
+
+        Raises
+        ------
+        ValueError: Entity name not found in score_dictionary
         """
         if entity_name == "Damsel":
             score_reduce: int = self.score_dictionary["damsel_death"]["score"]
@@ -134,3 +152,5 @@ class ScoreController(object):
                 self.boredom_meter = 0
             else:
                 self.boredom_meter -= score_reduce
+        else:
+            raise ValueError("Entity type not found in score_dictionary.")
