66b4991bdf63b93047f03d440487cc0c1174c323
owalk <oliverw56@gmail.com>
2024-02-22 13:14:58 -0500

    Update engine code to support the test enviroment
    These changes are a hotfix and will need to be further
    addressed in the future.

--- m1wengine/settings.py
+++ m1wengine/settings.py
@@ -17,11 +17,11 @@ ENTITY_HITBOX_OFFSET = -4
 
 # image paths
 GAME_ICON_PATH = "game_assets/graphics/game_icon.png"
-STUDIO_SPLASH_SCREEN_PATH = "game_assets/graphics/studio_logo.png"
-MAIN_MENU_BACKGROUND_PATH = "game_assets/graphics/main_menu/ground.png"
+STUDIO_SPLASH_SCREEN_PATH = "test/test_assets/graphics/studio_logo.png"
+MAIN_MENU_BACKGROUND_PATH = "test/test_assets/graphics/main_menu/ground.png"
 
 # Constant used to loop game music
 LOOP_MUSIC = -1
 
-CHARACTER_IMAGES = "game_assets/graphics/tiles/entities/characters/"
-ITEM_IMAGES = "game_assets/graphics/tiles/entities/items/"
+CHARACTER_IMAGES = "test/test_assets/graphics/tiles/entities/characters/"
+ITEM_IMAGES = "test/test_assets/graphics/tiles/entities/items/"
